ed9a6465d0799c2d7c2e55e027583daa32013ea2
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-05-07 12:09:18 +0200

    added functions

--- utils.py
+++ utils.py
@@ -5,12 +5,15 @@
 import arcpy as ap
 import numpy as np
 
+import logging
 import os
 
 CLASSLABELFORMATS = {
     'en': '{0:.{1:d}f} - {2:.{3:d}f}',
+    'msep': '{0:,.{1:d}f} - {2:,.{3:d}f}',
     'nl': '{0:.{1:d}f} tot {2:.{3:d}f}'}
 
+logging.basicConfig(level=logging.DEBUG)
 
 def safe_get(list_, idx=0, default=None):
     """get requested item of list if len(list) > (idx+1), else default"""
@@ -50,9 +53,9 @@ def fixlabels(layer, decimals=2, lang='en'):
     newlabels = []
     for left, right in zip(values[:-1], values[1:]):
         if left in {-999, -9999, -1e9}:
-            newlabels.append('< {0:.{1:d}f}'.format(right, decimals))
+            newlabels.append('< {0:,.{1:d}f}'.format(right, decimals))
         elif right in {999, 9999, 1e9}:
-            newlabels.append('> {0:.{1:d}f}'.format(left, decimals))
+            newlabels.append('> {0:,.{1:d}f}'.format(left, decimals))
         else:
             newlabels.append(fmt.format(left, decimals, right, decimals))
     lyr.symbology.classBreakLabels = newlabels
@@ -68,6 +71,26 @@ def namereplace(find, replace, grouplayer=None, wildcard='*'):
     for layer in layers:
         layer.name = layer.name.replace(find, replace)
     ap.RefreshTOC()
+
+
+def export_bookmarks(folder, fileformat='{name:}.pdf', dataframe='Layers'):
+    mxd = ap.mapping.MapDocument('CURRENT')
+    df = ap.mapping.ListDataFrames(mxd, dataframe)[0]
+    for bookmark in ap.mapping.ListBookmarks(mxd, data_frame=df):
+        df.extent = bookmark.extent
+        name = bookmark.name.replace(',', '').replace(':', '')
+        mapfile = os.path.join(folder, fileformat.format(name=name))
+        ap.mapping.ExportToPDF(mxd, mapfile)
+
+
+def export_pages_png(folder, fileformat='{pagename:}.png', dpi=200):
+    mxd = ap.mapping.MapDocument('CURRENT')
+    for pageNum in range(1, mxd.dataDrivenPages.pageCount + 1):
+        mxd.dataDrivenPages.currentPageID = pageNum
+        pagename = mxd.dataDrivenPages.pageRow.Name
+        filename = os.path.join(folder, fileformat.format(pagename=pagename))
+        logging.info('exporting to {}'.format(os.path.basename(filename)))
+        ap.mapping.ExportToPNG(mxd, filename, resolution=dpi)
 
 
 def clip_by_display(features, dataframe):
@@ -86,6 +109,30 @@ def clip_by_display(features, dataframe):
     else:
         output_features = '{}_{}.{}'.format(filename, dataframe, ext)
     ap.analysis.Clip(features, extent_mask, output_features)
+
+
+def unique_values(table, field):
+    with ap.da.SearchCursor(table, [field]) as cursor:
+        return sorted({safe_get(row, 0) for row in cursor})
+
+def splitby(layer, keyfield):
+    fields = ap.ListFields(layer)
+    field = safe_get([f for f in fields if f.name == keyfield], 0)
+    if not field.type in {'String', 'Int'}:
+        raise ValueError('{} field not allowed as key'.format(field.type))
+    if not isinstance(layer, ap.mapping.Layer):
+        layer = ap.mapping.Layer(layer)
+    for value in unique_values(layer, keyfield):
+        logging.info('selecting {} = {}'.format(keyfield, value))
+        query = '"{field:}" = \'{value:}\''.format(field=keyfield, value=value)
+        ap.management.SelectLayerByAttribute(layer, 'NEW_SELECTION', query)
+        filename, ext = os.path.splitext(layer.dataSource)
+        filepath = os.path.join('{filename:}_{value:}{ext:}'.format(
+            filename=filename,
+            value=value,
+            ext=ext,))
+        logging.info('exporting to {}'.format(filepath))
+        ap.management.CopyFeatures(layer, filepath)
 
 
 def setdatasource(layer, datasource):
